9dfe3ea84b49e4229d2e77b05a91b6748450d05f
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-04-08 15:57:45 +0300

    theme-33

--- wp-content/themes/ksm/index.php
+++ wp-content/themes/ksm/index.php
@@ -1218,7 +1218,7 @@
 
                             <div class="servicelist-item"> 
 
-                                <div class="icon"><div><i class="stream-icon stream-icon-clock"></i></div></div>
+                                <div class="icon"><div><i class="stream-icon stream-icon-rocket"></i></div></div>
 
                                 <!-- Servicelist Icon -->
 
