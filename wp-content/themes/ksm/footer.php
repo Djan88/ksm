398686a81b879fa792affc5c69f1d70aae229a61
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-04-09 11:43:42 +0300

    theme-48

--- wp-content/themes/ksm/footer.php
+++ wp-content/themes/ksm/footer.php
@@ -6,45 +6,53 @@
           <div class="container">
             <!-- BOF Footer widget #1 -->
             <div class="four columns">
-
-            </div>                
-            <!-- EOF Footer widget #1 -->
-            <!-- BOF Footer widget #2-->
-            <div class="four columns">
-            
-            </div>                
-            <!-- EOF Footer widget #2-->
-            <!-- BOF Footer widget #3 -->
-            <div class="four columns">
-            
-            </div>                
-            <!-- EOF Footer widget #3-->
-            <!-- BOF Footer widget #4 -->
-            <div class="four columns">
               <div class="widget">
-                <div class="jx-footer-title">Contacts</div>
                 <!-- widget Title -->                         
                 <div class="jx-footer-address">
                   <ul>
                     <li>
                       <i class="line-icon icon-location"></i>
-                      <div>Bolean, 959 Road, Avenue Street, Buston, USA</div>
-                    </li>
-                    <li>
-                      <i class="line-icon icon-mobile"></i>
-                      <div class="tel"><strong>Tel :</strong>  00965 - 3426 - 3211223</div>
-                      <div class="fax"><strong>Fax :</strong> 00965 - 3426 - 3211223</div>
-                    </li>
-                    <li>
-                      <i class="line-icon icon-globe"></i>
-                      <div class="email"><strong>Email :</strong> Name@your-company.com</div>
+                      <div>РФ, Москва, ул. Центральная, 15</div>
                     </li>
                   </ul>
                 </div>
                 <!-- Contact Address -->
-              </div> 
-            </div>                  
-            <!-- EOF Footer widget #4 -->
+              </div>
+            </div>                
+            <!-- EOF Footer widget #1 -->
+            <!-- BOF Footer widget #2-->
+            <div class="four columns">
+              <div class="widget">
+                <!-- widget Title -->                         
+                <div class="jx-footer-address">
+                  <ul>
+                    <li>
+                      <i class="line-icon icon-mobile"></i>
+                      <div class="tel"><strong>Tel :</strong>  8(495) - 3426 - 3211223</div>
+                      <div class="fax"><strong>Fax :</strong> 8(495) - 3426 - 3211223</div>
+                    </li>
+                  </ul>
+                </div>
+                <!-- Contact Address -->
+              </div>
+            </div>                
+            <!-- EOF Footer widget #2-->
+            <!-- BOF Footer widget #3 -->
+            <div class="four columns">
+              <div class="widget">
+                <!-- widget Title -->                         
+                <div class="jx-footer-address">
+                  <ul>
+                    <li>
+                      <i class="line-icon icon-globe"></i>
+                      <div class="email"><strong>Email :</strong> company@your-company.com</div>
+                    </li>
+                  </ul>
+                </div>
+                <!-- Contact Address -->
+              </div>
+            </div>                
+            <!-- EOF Footer widget #3-->
           </div>
         </div>
         <!-- EDF widget FOOTER -->
@@ -52,7 +60,7 @@
         <div class="jx-sub-footer jx-container">
           <div class="container"> 
             <div class="eight columns">
-              <div class="jx-copy-right">Copyright © 2015 Powered By <a href="#">Janxcode -</a> REBUILD Theme - All Rights Reserved</div>
+              <div class="jx-copy-right">Все права защищены © 2017 Разработано в  <a href="http://bablosstudio.ru/">Bablos Studio -</a></div>
             </div>                
           <!-- Copyright Text -->
             <div class="eight columns">
@@ -75,27 +83,12 @@
                   </li>
                 </ul>
               </div>
-
-                       
-
-                       
-
-                    </div>
-
-                    <!-- Social Icons -->                
-
-                
-
-                </div>
-
             </div>
-
-            <!-- EDF SUB FOOTER -->        
-
-    
-
-        </div>        
-
+            <!-- Social Icons -->                
+          </div>
+        </div>
+      <!-- EDF SUB FOOTER -->        
+      </div>        
     </footer>
     <!-- EOF FOOTER -->
     <!-- Footer -->
