acb25ae31dfd7ac820528d5ebad5f1b8e29f0fd8
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-04-09 11:38:35 +0300

    theme-47

--- wp-content/themes/ksm/footer.php
+++ wp-content/themes/ksm/footer.php
@@ -1,473 +1,80 @@
     <!-- BOF FOOTER -->
     <footer class="jx-footer-section">
-
-    <div class="jx-footer-1">
-
-        
-
-            <!-- BDF widget FOOTER -->        
-
-            <div class="jx-footer jx-container">
-
-                <div class="container">
-
-                
-
-                    <!-- BOF Footer widget #1 -->
-
-                    <div class="four columns">
-
-       
-
-                        <div class="widget">             
-
-                            <div class="jx-footer-logo">
-
-                                <a href="index.html"><img src="images/header_logo.png" alt="" /></a>
-
-                            </div>
-
-                            <!-- footer logo -->
-
-                            <div class="jx-about">
-
-                            <div class="jx-title-border"></div>
-
-                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus.</p>
-
-                            <!-- Content -->
-
-                            <div class="jx-btn jx-black"> 
-
-                                <div class="jx-btn jx-black"> 
-
-                                    <a href="about-us.html" class="jx-btn-default">
-
-                                        <span>  
-
-                                            <i class="btn-icon-left fa fa-mail-forward"></i>
-
-                                            <span>Read More</span>
-
-                                            <i class="btn-icon-right fa fa-mail-forward"></i>
-
-                                        </span>
-
-                                    </a>
-
-                              </div>
-
-                            </div>
-
-                            </div>
-
-                            
-
-                        </div>
-
-                        <!-- About Us -->
-
-                            <div class="jx-border"></div>
-
-                        
-
-                        <div class="widget">
-
-                            <div class="jx-footer-newsletter">
-
-                                <div class="jx-footer-title">Newsletter Subscribe</div>
-
-                                <!-- widget Title -->                         
-
-                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>                             
-
-                            
-
-                                <form action="#" id="newsletter" method="post" class="jx-form-wrapper">
-
-                                    <div id="newsletter-email" class="search-inline-block">
-
-                                        <input  id="newsletter-email-1" name="newsletter_email" placeholder="Newsletter Subscribe" class="jx-form-name" type="text">
-
-                                    </div>
-
-                                    <div id="newsletter-submit">
-
-                                        <button type="submit"><i class="fa fa-envelope-o"></i></button>
-
-                                    <!-- Submit Button -->  
-
-                                    </div>
-
-                                </form>
-
-                                
-
-                             </div>
-
-                        </div> 
-
-                        <!-- Newsletter Subscribe -->
-
-                    </div>                
-
-                    <!-- EOF Footer widget #1 -->
-
-                    
-
-                    <!-- BOF Footer widget #2-->
-
-                    <div class="four columns">
-
-                      <div class="widget">
-
-                            <div class="jx-footer-title">Recent post</div>
-
-                            <!-- widget Title -->
-
-                            <div class="jx-widget-recent-post">
-
-                                <ul>
-
-                                    <li>
-
-                                        <div class="image"><img src="images/blog-s-1.jpg" alt="" /></div>
-
-                                        <div class="post-content">
-
-                                            <div class="title"><a href="news-single-page.html">Responsive HTML Design</a></div>
-
-                                            <div class="date">29 JUN 2015</div>
-
-                                      </div>
-
-                                    </li>
-
-                                    <li>
-
-                                        <div class="image"><img src="images/blog-s-2.jpg" alt="" /></div>
-
-                                         <div class="post-content">
-
-                                            <div class="title"><a href="news-single-page.html">Clean and Modern</a></div>
-
-                                            <div class="date">02 AUG 2015</div>
-
-                                        </div>
-
-                                    </li>
-
-                                    <li>
-
-                                        <div class="image"><img src="images/blog-s-3.jpg" alt="" /></div>
-
-                                         <div class="post-content">
-
-                                            <div class="title"><a href="news-single-page.html">Unlimited Control Options</a></div>
-
-                                            <div class="date">20 JAN 2015</div>
-
-                                        </div>
-
-                                    </li>
-
-                                    <li>
-
-                                        <div class="image"><img src="images/blog-s-4.jpg" alt="" /></div>
-
-                                         <div class="post-content">
-
-                                            <div class="title"><a href="news-single-page.html">Responsive HTML Design</a></div>
-
-                                            <div class="date">20 JAN 2015</div>
-
-                                        </div>
-
-                                    </li>
-
-                                </ul>
-
-                                <div class="jx-btn jx-black"> 
-
-                                    <a href="news.html" class="jx-btn-default">
-
-                                        <span>  
-
-                                            <i class="btn-icon-left fa fa-mail-forward"></i>
-
-                                            <span>Read More</span>
-
-                                            <i class="btn-icon-right fa fa-mail-forward"></i>
-
-                                        </span>
-
-                                    </a>
-
-                              </div>
-
-                            </div>
-
-                            <!-- Recent Post -->
-
-                        </div>
-
-                    </div>                
-
-                    <!-- EOF Footer widget #2-->
-
-                    
-
-                    <!-- BOF Footer widget #3 -->
-
-                    <div class="four columns">
-
-                    
-
-                      <div class="widget">                
-
-                            <div class="jx-footer-title">Tags</div>
-
-                            <!-- widget Title -->  
-
-                            
-
-                            <div class="jx-tags-footer">
-
-                                <ul>
-
-                                    <li>responsive</li>
-
-                                    <li>jquery</li>
-
-                                    <li>css</li>
-
-                                    <li>design</li>
-
-                                    <li>graphic</li>
-
-                                    <li>php</li>
-
-                                    <li>html5</li>
-
-                                    <li>jquery</li>
-
-                                    <li>Css</li>
-
-                                </ul>
-
-                            </div>
-
-                            
-
-            </div>
-
-                        <!-- Tags Group -->                      
-
-                        <div class="jx-border"></div>
-
-                      <div class="widget">                
-
-                            <div class="jx-footer-title">Flickr</div>
-
-                            <!-- widget Title -->  
-
-                            
-
-                            <div class="jx-footer-flickr">
-
-                                <ul>
-
-                                    <li><a href="images/portfolio-w-1.jpg" data-rel="prettyPhoto"><img src="images/flick-1.jpg" alt="" /></a></li>
-
-                                    <!-- item 01 -->
-
-                                    <li><a href="images/portfolio-w-2.jpg" data-rel="prettyPhoto"><img src="images/flick-2.jpg" alt="" /></a></li>
-
-                                    <!-- item 01 -->
-
-                                    <li><a href="images/portfolio-w-3.jpg" data-rel="prettyPhoto"><img src="images/flick-3.jpg" alt="" /></a></li>
-
-                                    <!-- item 01 -->
-
-                                    <li><a href="images/portfolio-w-4.jpg" data-rel="prettyPhoto"><img src="images/flick-4.jpg" alt="" /></a></li>
-
-                                    <!-- item 01 -->
-
-                                    <li><a href="images/portfolio-w-5.jpg" data-rel="prettyPhoto"><img src="images/flick-5.jpg" alt="" /></a></li>
-
-                                    <!-- item 01 -->
-
-                                    <li><a href="images/portfolio-w-6.jpg" data-rel="prettyPhoto"><img src="images/flick-6.jpg" alt="" /></a></li>
-
-                                    <!-- item 01 -->
-
-                                </ul>    
-
-                            </div>
-
-                            
-
-            </div>
-
-                        <!-- Flickr -->
-
-                    
-
-                    </div>                
-
-                    <!-- EOF Footer widget #3-->
-
-                    
-
-                    <!-- BOF Footer widget #4 -->
-
-                    <div class="four columns">
-
-                    
-
-                      <div class="widget">
-
-                            <div class="jx-footer-title">Video</div>
-
-                            <!-- widget Title -->                         
-
-                          <iframe height="160" src="https://www.youtube.com/embed/hr3QyhiD50k" ></iframe>
-
-                        </div> 
-
-                        
-
-                        <div class="widget">
-
-                            <div class="jx-footer-title">Contacts</div>
-
-                            <!-- widget Title -->                         
-
-                            
-
-                            <div class="jx-footer-address">
-
-                                <ul>
-
-                                    <li>
-
-                                    <i class="line-icon icon-location"></i>
-
-                                    <div>Bolean, 959 Road, Avenue Street, Buston, USA</div>
-
-                                    </li>
-
-                                    <li>
-
-                                    <i class="line-icon icon-mobile"></i>
-
-                                    <div class="tel"><strong>Tel :</strong>  00965 - 3426 - 3211223</div>
-
-                                    <div class="fax"><strong>Fax :</strong> 00965 - 3426 - 3211223</div>
-
-                                    </li>
-
-                                    <li>
-
-                                    <i class="line-icon icon-globe"></i>
-
-                                    <div class="email"><strong>Email :</strong> Name@your-company.com</div>
-
-                                    </li>
-
-                                </ul>
-
-                            </div>
-
-                            <!-- Contact Address -->
-
-                        </div> 
-
-                                         
-
-                    </div>                  
-
-                    <!-- EOF Footer widget #4 -->
-
-                </div>
-
-            </div>
-
+      <div class="jx-footer-1">
+        <!-- BDF widget FOOTER -->        
+        <div class="jx-footer jx-container">
+          <div class="container">
+            <!-- BOF Footer widget #1 -->
+            <div class="four columns">
+
+            </div>                
+            <!-- EOF Footer widget #1 -->
+            <!-- BOF Footer widget #2-->
+            <div class="four columns">
             
-
-            <!-- EDF widget FOOTER -->
-
-    
-
-    
-
-            <!-- BDF SUB FOOTER -->        
-
-            <div class="jx-sub-footer jx-container">
-
-                <div class="container"> 
-
-                
-
-                    <div class="eight columns">
-
-                        <div class="jx-copy-right">Copyright © 2015 Powered By <a href="#">Janxcode -</a> REBUILD Theme - All Rights Reserved</div>
-
-                    </div>                
-
+            </div>                
+            <!-- EOF Footer widget #2-->
+            <!-- BOF Footer widget #3 -->
+            <div class="four columns">
+            
+            </div>                
+            <!-- EOF Footer widget #3-->
+            <!-- BOF Footer widget #4 -->
+            <div class="four columns">
+              <div class="widget">
+                <div class="jx-footer-title">Contacts</div>
+                <!-- widget Title -->                         
+                <div class="jx-footer-address">
+                  <ul>
+                    <li>
+                      <i class="line-icon icon-location"></i>
+                      <div>Bolean, 959 Road, Avenue Street, Buston, USA</div>
+                    </li>
+                    <li>
+                      <i class="line-icon icon-mobile"></i>
+                      <div class="tel"><strong>Tel :</strong>  00965 - 3426 - 3211223</div>
+                      <div class="fax"><strong>Fax :</strong> 00965 - 3426 - 3211223</div>
+                    </li>
+                    <li>
+                      <i class="line-icon icon-globe"></i>
+                      <div class="email"><strong>Email :</strong> Name@your-company.com</div>
+                    </li>
+                  </ul>
+                </div>
+                <!-- Contact Address -->
+              </div> 
+            </div>                  
+            <!-- EOF Footer widget #4 -->
+          </div>
+        </div>
+        <!-- EDF widget FOOTER -->
+        <!-- BDF SUB FOOTER -->        
+        <div class="jx-sub-footer jx-container">
+          <div class="container"> 
+            <div class="eight columns">
+              <div class="jx-copy-right">Copyright © 2015 Powered By <a href="#">Janxcode -</a> REBUILD Theme - All Rights Reserved</div>
+            </div>                
           <!-- Copyright Text -->
-
-                    
-
-                    <div class="eight columns">
-
-                       
-
-                       <div class="jx-footer-social-icon">
-
-                        <ul>                            
-
-                            <li class="facebook">
-
-                            <a href="http://www.facebook.com/#" target="_blank"><i class="fa fa-facebook"></i></a>
-
-                            </li>
-
-                           
-
-                            <li class="twitter">
-
-                            <a href="http://www.twitter.com/#" target="_blank"><i class="fa fa-twitter"></i></a>
-
-                            </li>
-
-                            
-
-                            <li class="dribbble">
-
-                            <a href="http://www.dribbble.com/#" target="_blank"><i class="fa fa-dribbble"></i></a>
-
-                            </li>
-
-                                                                                   
-
-                            <li class="youtube">
-
-                            <a href="http://www.youtube.com/#" target="_blank"><i class="fa fa-youtube"></i></a>
-
-                            </li>
-
-                            
-
-                            <li class="googleplus">
-
-                            <a href="http://www.googleplus.com/#" target="_blank"><i class="fa fa-google-plus"></i></a>
-
-                            </li>
-
-                        </ul>
-
-                     </div>
+            <div class="eight columns">
+              <div class="jx-footer-social-icon">
+                <ul>                            
+                  <li class="facebook">
+                    <a href="http://www.facebook.com/#" target="_blank"><i class="fa fa-facebook"></i></a>
+                  </li>
+                  <li class="twitter">
+                    <a href="http://www.twitter.com/#" target="_blank"><i class="fa fa-twitter"></i></a>
+                  </li>
+                  <li class="dribbble">
+                    <a href="http://www.dribbble.com/#" target="_blank"><i class="fa fa-dribbble"></i></a>
+                  </li>
+                  <li class="youtube">
+                    <a href="http://www.youtube.com/#" target="_blank"><i class="fa fa-youtube"></i></a>
+                  </li>
+                  <li class="googleplus">
+                    <a href="http://www.googleplus.com/#" target="_blank"><i class="fa fa-google-plus"></i></a>
+                  </li>
+                </ul>
+              </div>
 
                        
 
