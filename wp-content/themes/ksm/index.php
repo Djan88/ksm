50f5d8e7f7f20cab02bd3df1f9baad7289a335ae
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-04-13 12:00:27 +0300

    theme ksm-39

--- wp-content/themes/ksm/index.php
+++ wp-content/themes/ksm/index.php
@@ -88,123 +88,48 @@
         <!-- EOF Blog -->      
       </div>
       <!-- EOF Body Content -->        
-      <div id="sidebar" class="four columns right omega">        
-        <div class="jx-sidebar-block mb40">          
-          <div class="jx-section-title-2">
-            <div class="jx-title jx-uppercase small-text">Поиск</div>
-            <div class="jx-seperator-hr"></div>
-          </div>
-          <!-- Section title -->             
-          <div class="jx-sidebar-search">
-            <form action="#" id="contactForm-1" method="post" class="jx-form-wrapper cf">                                
-              <div class="search-input search-inline-block">
-                <input type="text" id="search-item" name="search_item" placeholder="Search..." class="jx-form-name" />                                
-                <button type="submit"><i class="fa fa-search"></i></button>
-              </div>
-            </form>                        
-          </div>
-          <!-- Search -->
-        </div>
+      <div id="sidebar" class="four columns right omega">
         <!-- Widget 01 -->          
         <div class="jx-sidebar-block mb40">          
           <div class="jx-section-title-2">
-            <div class="jx-title jx-uppercase small-text">Recent Posts</div>
+            <div class="jx-title jx-uppercase small-text">Последние новости</div>
             <div class="jx-seperator-hr"></div>
           </div>
         <!-- Section title --> 
           <div class="jx-sidebar-recent-post">
             <ul>
               <li>
-                <div class="image"><img src="images/blog-s-1.jpg" alt=""></div>
+                <div class="image"><img src="<?php bloginfo('template_url'); ?>/images/stock-3.jpg" alt=""></div>
                 <div class="post-content">
                   <div class="title">
-                    <a href="news-single-page.html">Lorem ipsum dolor sit</a>
+                    <a href="#">Коттеджный поселок</a>
                   </div>
-                  <div class="date">29 JUL 2015</div>
+                  <div class="date">25 Янв 2017</div>
                 </div>
-              </li>                              
+              </li>
               <li>
-                <div class="image"><img src="images/blog-s-2.jpg" alt=""></div>
+                <div class="image"><img src="<?php bloginfo('template_url'); ?>/images/stock-4.jpg" alt=""></div>
                 <div class="post-content">
-                  <div class="title"><a href="news-single-page.html">Nulla consequat massa</a></div>
-                  <div class="date">18 JUN 2015</div>
+                  <div class="title">
+                    <a href="#">ЖК «Садовые кварталы»</a>
+                  </div>
+                  <div class="date">26 Янв 2017</div>
                 </div>
-              </li>                
+              </li>
               <li>
-                <div class="image"><img src="images/blog-s-3.jpg" alt=""></div>
+                <div class="image"><img src="<?php bloginfo('template_url'); ?>/images/stock-5.jpg" alt=""></div>
                 <div class="post-content">
-                  <div class="title"><a href="news-single-page.html">Vivamus elementum semper</a></div>
-                  <div class="date">15 May 2015</div>
+                  <div class="title">
+                    <a href="#">Жилой комплекс Доминион</a>
+                  </div>
+                  <div class="date">27 Марта 2017</div>
                 </div>
-              </li>                
+              </li>               
             </ul>
           </div>
           <!-- Recent Post -->
         </div>
-        <!-- Widget 02 -->          
-        <div class="jx-sidebar-block mb40 widget-area">          
-          <div class="jx-section-title-2">
-            <div class="jx-title jx-uppercase small-text">Categories</div>
-            <div class="jx-seperator-hr"></div>
-          </div>
-          <!-- Section title -->             
-          <div class="jx-sidebar-categories">    
-            <ul>
-              <li><a href="#">Construction</a></li>
-              <li><a href="#">House Maintenance</a></li>
-              <li><a href="#">Carpentry</a></li>
-              <li><a href="#">Hardware</a></li> 
-              <li><a href="#">Buildings</a></li>
-              <li><a href="#">Modern Kitchens</a></li>                         
-            </ul>                          
-          </div>
-          <!-- Categories -->
-        </div>
-        <!-- Widget 03 -->
-        <div class="jx-sidebar-block mb40 widget-area">          
-          <div class="jx-section-title-2">
-            <div class="jx-title jx-uppercase small-text">Tags</div>
-            <div class="jx-seperator-hr"></div>
-          </div>
-          <!-- Section title -->               
-          <div class="jx-sidebar-tags">                          
-            <ul>
-              <li><a href="#">Construction</a></li>
-              <li><a href="#">House Maintenance</a></li>
-              <li><a href="#">Carpentry</a></li>
-              <li><a href="#">Hardware</a></li> 
-              <li><a href="#">Buildings</a></li>
-              <li><a href="#">Modern Kitchens</a></li>                         
-            </ul>                                     
-          </div>
-          <!-- Categories -->
-        </div>
-        <!-- Widget 03 -->                      
-        <div class="jx-sidebar-block mb40">                  
-          <div class="jx-section-title-2">
-            <div class="jx-title jx-uppercase small-text">Testimonials</div>
-            <div class="jx-seperator-hr"></div>
-          </div>
-          <!-- Section title -->             
-          <div class="jx-sidebar-testimonials">                          
-            <ul>
-              <li>
-                <div class="jx-tetimonials-1">                      
-                  <div class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam...</div>
-                  <div class="jx-testimonial-details">
-                    <div class="jx-testimonial-image">
-                      <img src="images/team_5.jpg" alt="testimonials images">
-                    </div>
-                    <div class="name">Alex Simon</div>
-                    <div class="position">Maxon Corp</div>
-                  </div>                        
-                </div>                        
-              </li>                        
-            </ul>                                   
-          </div>
-          <!-- Testimonials -->
-        </div>
-        <!-- Widget 03 -->                          
+        <!-- Widget 02 -->                        
       </div>
     <!-- EOF Sidebar -->          
     </div>
